97409ac65e09ee8853f9199ebbe722f54d0e7deb
nanireo <61157090+nanireo@users.noreply.github.com>
2024-10-07 22:08:14 +0530

    samuel

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa'; // Importing menu and close icons from react-icons
+import {BrowserRouter, Routes, Route,Link } from 'react-router-dom'
 
 function Header() {
     const [isOpen, setIsOpen] = useState(false);
@@ -27,13 +28,22 @@ function Header() {
             <header className={`nav-links ${isOpen ? 'open' : ''}`}>
                 <nav>
                     <ul>
-                        <li><a href="/Home">Home</a></li>
-                        <li><a href="#about">About</a></li>
+                    <Link to = "/Home"><p class='HeaderP'>Home</p></Link> 
+                    <Link to = "#about"><p class='HeaderP'>About</p></Link> 
+                    <Link to = "#services"><p class='HeaderP'>Gallery</p></Link> 
+                    <Link to = "#contact"><p class='HeaderP'>Our Branches</p></Link> 
+                    <Link to = "#departments"><p class='HeaderP'>Vision</p></Link> 
+                    <Link to = "#academics"><p class='HeaderP'>Downloads</p></Link> 
+                    <Link to = "/ContactUs"><p class='HeaderP'>Contact Us</p></Link> 
+
+                        {/* <li><a href="/Home">Home</a></li> */}
+                        {/* <li><a href="#about">About</a></li>
                         <li><a href="#services">Gallery</a></li>
                         <li><a href="#contact">Our Branches</a></li>
                         <li><a href="#departments">Vision</a></li>
-                        <li><a href="#academics">Downloads</a></li>
-                        <li><a href="/ContactUs">Contact Us</a></li>
+                        <li><a href="#academics">Downloads</a></li> */}
+
+                        {/* <li><a href="/ContactUs">Contact Us</a></li> */}
                     </ul>
                 </nav>
             </header>
