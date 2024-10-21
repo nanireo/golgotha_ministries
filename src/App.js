2b4b1da85eef5c4a3b732140c0358bb99553deb8
nanireo <61157090+nanireo@users.noreply.github.com>
2024-10-21 10:55:33 +0530

    samuel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+
 // No need for hash-based routing now
 
 import './css/Header.css';
@@ -30,7 +31,7 @@ function App() {
                     <Route path="/Home" element={<Home />} />
                     <Route path="/ContactUs" element={<ContactUs />} />
                 </Routes>
-
+                
                 <Footer />
             </Router>
         </div>
