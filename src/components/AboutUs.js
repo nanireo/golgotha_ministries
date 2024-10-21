8a30e63873aae1af8f58721788f7188c3d22e7fa
nanireo <61157090+nanireo@users.noreply.github.com>
2024-10-21 22:29:14 +0530

    samuel

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -2,24 +2,26 @@ import React from 'react';
 
 const AboutUs = () => {
     return (
-        <div className="about-us-container">
-            <div className="about-us-image">
+        <> 
+            <div className="about-image-section">
                 <img src="church pics/14 c 2.jpeg" alt="Brother Samuel Mories" />
             </div>
-            <div className="about-us-content">
-                <h1>🌟 About Us 🌟</h1>
-                <p>
-                    Golgotha Ministries is a beacon of faith and hope in the heart of Armoor, founded 26 years ago by the dedicated and passionate Pastor B.N. Rathnam. Our mission is simple yet profound: to share the gospel and win souls for Christ.
-                </p>
-                <h2>💫 Our Vision 💫</h2>
-                <p>
-                    We believe in spreading the message of love, redemption, and salvation to all, guided by the teachings of Jesus Christ. Our vision is to create a community of believers who are not only spiritually enriched but also actively engaged in making a positive impact on our world. Alongside him, we are privileged to have his son, Brother Samuel Mories, who is actively contributing as an associate in the ministry.
-                </p>
-                <p>
-                    Stay connected with us on this platform to receive spiritual nourishment, engage in meaningful discussions, and participate in the wonderful journey of faith with Golgotha Ministries.
-                </p>
+            <div className="about-content-section">
+                <div className="about-text">
+                    <h1>🌟 About Us 🌟</h1>
+                    <p>
+                        Golgotha Ministries is a beacon of faith and hope in the heart of Armoor, founded 26 years ago by the dedicated and passionate Pastor B.N. Rathnam. Our mission is simple yet profound: to share the gospel and win souls for Christ.
+                    </p>
+                    <h2>💫 Our Vision 💫</h2>
+                    <p>
+                        We believe in spreading the message of love, redemption, and salvation to all, guided by the teachings of Jesus Christ. Our vision is to create a community of believers who are not only spiritually enriched but also actively engaged in making a positive impact on our world. Alongside him, we are privileged to have his son, Brother Samuel Mories, who is actively contributing as an associate in the ministry.
+                    </p>
+                    <p>
+                        Stay connected with us on this platform to receive spiritual nourishment, engage in meaningful discussions, and participate in the wonderful journey of faith with Golgotha Ministries.
+                    </p>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
